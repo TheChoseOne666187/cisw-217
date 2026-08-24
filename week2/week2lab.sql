d134c874b84093f8854821aa8e9d06d0ddf66162
TheChoseOne666187 <nmacmanus@csi.edu>
2026-08-24 12:02:35 -0600

    Create week2lab.sql

---
+++ week2/week2lab.sql
@@ -0,0 +1,6 @@
+Create Table Animals(Animals_id Bigserial Primary key, Animal_name TEXT, species_id INT, age INT)
+INSERT INTO animals(species_name, species_habitat, species_diet)
+Values
+(1,' Simba the Lion','Jungle','Prey'),
+(2,'Melman the Giraffe','Plains','Leaves'),
+(3,'Skipper the Penguin', 'The_Arctic','Fish');
